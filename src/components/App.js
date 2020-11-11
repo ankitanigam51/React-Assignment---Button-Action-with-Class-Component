63208d24a7129ef8aeaf7b21d9bfff857183991e
Ankita Nigam <ankitanigam51@gmail.com>
2020-11-11 19:12:31 +0530

    Assignment Q5

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,19 @@ class App extends Component {
     constructor(props) {
 		super(props);
 	};
+   
+	function add() {
+		const res =document.getElementsByTagName("p");
+		console.log(res);
+	} 
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				{ /* Do not remove this main div!! */
+				<p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+				<button id="para" onClick="add()">Click Me!!</button>
+				}
     		</div>
     	);
     }
